1002b394dc2aeae491fd206b11416fbba393c7eb
teknogrot <arquinsiel@gmail.com>
2020-12-06 19:37:39 +0000

    Core functionality working correctly. All echo errors tidied up. TODO: usage message.

--- nmaper.sh
+++ nmaper.sh
@@ -3,9 +3,13 @@
 # A simple script to automate the port enumeration process as described on the Hack The Box website #
 
 input=$1
-output=$1+"_results"
+echo "Target host is $input."
+output=$1"_results"
+echo "Open ports will be saved to $output.txt."
+echo "Running port enumeration."
 ports=$(nmap -p- --min-rate=1000 -T4 $input | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
 echo "Ports found on $input are: $ports" > $output.txt
+echo "Running service detection."
 nmap -sC -sV -p$ports $input -oA $input
 
 usage()
